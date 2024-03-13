50fb732bb6f512007edeeb69fa2a0d9ceaa85e6b
Daniel Terry <Daniel.N.Terry@hotmail.com>
2024-03-13 16:45:58 +1100

    Add GUI

--- js/script.js
+++ js/script.js
@@ -10,44 +10,108 @@ function getComputerChoice() {
 }
 
 function gameRound(playerSelection, computerSelection) {
-    let result = ""
     switch (playerSelection) {
         case 'rock':
             if (computerSelection === 'Rock') {
-                result = "Tie - Rock vs Rock"
+                return 0;
             } else if (computerSelection === 'Scissors') {
-                result = "You Win! Rock beats Scissors"
+                return 1;
             } else {
-                result = "You Lose! Paper beats Rock"
+                return -1;
             }
         case 'scissors':
             if (computerSelection === 'Rock') {
-                result = "You Lose! Rock beats Scissors"
+                return -1;
             } else if (computerSelection === 'Scissors') {
-                result = "Tie - Scissors vs Scissors"
+                return 0;
             } else {
-                result = "You Win! Scissors beats Paper"
+                return 1;
             }
         case 'paper':
             if (computerSelection === 'Rock') {
-                result = "You Win! Paper beats Rock"
+                return 1;
             } else if (computerSelection === 'Scissors') {
-                result = "You Lose! Scissors beats Paper"
+                return -1;
             } else {
-                result = "Tie - Paper vs Paper"
+                return 0;
             }
     }
-    return result;
+    return 0;
 }
 
 function playGame() {
     let roundResult = "";
-    for (let i = 0; i < 5; i++) {
-        let userPrompt = prompt("Rock, Paper, Scissors? ")
-        let computer = getComputerChoice()
-        roundResult = gameRound(userPrompt.toLowerCase(), computer)
-        console.log(roundResult);
+    let userPrompt = prompt("Rock, Paper, Scissors? ")
+    let computer = getComputerChoice()
+    roundResult = gameRound(userPrompt.toLowerCase(), computer)
+}
+
+const rockBtn = document.getElementById('rock')
+const paperBtn = document.getElementById('paper')
+const scissorsBtn = document.getElementById('scissors')
+
+const resultDiv = document.getElementById("results")
+const playerScoreDiv = document.getElementById('player-score');
+const compScoreDiv = document.getElementById('comp-score');
+const roundWinnerDiv = document.getElementById('round-winner');
+
+const MAX_POINTS = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+const updateResults = (pChoice, cChoice, result) => {
+    switch (result) {
+        case 0:
+            roundWinnerDiv.innerHTML = "Tie " + pChoice + " vs " + cChoice;
+            break;
+        case 1:
+            playerScore++;
+            roundWinnerDiv.innerHTML = "WIN! " + pChoice + " beats " + cChoice;
+            break;
+        case -1:
+            computerScore++;
+            roundWinnerDiv.innerHTML = "LOSE! " + cChoice + " beats " + pChoice;
+    }
+    playerScoreDiv.innerHTML = "PLAYER: " + playerScore;
+    compScoreDiv.innerHTML = "CPU: " + computerScore;
+
+
+    if (playerScore >= MAX_POINTS || computerScore >= MAX_POINTS) {
+        const winner = document.createElement('div')
+        winner.innerHTML = playerScore > computerScore ? "Player Wins!" : "Computer Wins!"
+        resultDiv.appendChild(winner);
+        const resetBtn = document.createElement('button')
+        resetBtn.innerHTML = "Retry?";
+        resetBtn.addEventListener("click", () => {
+            playerScore = 0;
+            computerScore = 0;
+            resultDiv.removeChild(winner);
+            playerScoreDiv.innerHTML = "PLAYER: " + playerScore;
+            compScoreDiv.innerHTML = "CPU: " + computerScore;
+            roundWinnerDiv.innerHTML = "";
+            resultDiv.removeChild(resetBtn);
+        })
+        resultDiv.appendChild(resetBtn);
     }
 }
 
-playGame();
+rockBtn.addEventListener("click", () => {
+    if (playerScore >= MAX_POINTS || computerScore >= MAX_POINTS) return;
+    let compChoice = getComputerChoice()
+    const result = gameRound('rock', compChoice)
+    updateResults('Rock', compChoice, result)
+})
+
+paperBtn.addEventListener("click", () => {
+    if (playerScore >= MAX_POINTS || computerScore >= MAX_POINTS) return;
+    let compChoice = getComputerChoice()
+    const result = gameRound("paper", compChoice);
+    updateResults('Paper', compChoice, result);
+})
+
+scissorsBtn.addEventListener("click", () => {
+    if (playerScore >= MAX_POINTS || computerScore >= MAX_POINTS) return;
+    let compChoice = getComputerChoice();
+    const result = gameRound("paper", compChoice);
+    updateResults("Scissors", compChoice, result);
+})
